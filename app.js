1e7f1dc87cbe870ebff6db1c7d4a3d49972ba317
aalesann <alejandrosanabria74@gmail.com>
2022-02-27 11:25:49 -0300

    Primer deploy 27-02-2022

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Importaciones de librerías
 const express = require('express');
+const path = require('path');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const cors = require('cors');
@@ -7,7 +8,7 @@ const cors = require('cors');
 // Inicializaciones
 const app = express();
 require('dotenv').config();
-require('./database');
+// require('./database');
 
 // Middlewares
 app.use(helmet());
@@ -19,9 +20,13 @@ app.use(express.urlencoded({ extended: false }));
 // Configs
 const port = process.env.PORT || 5000;
 
+// Statics files
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Rutas
 app.use('/api/user',require('./routes/users.routes'));
 app.use('/api/auth',require('./routes/auth.routes'));
+app.use(require('./routes/demo.routes'));
 
 // Servidor en escucha
 app.listen(port, ()=> console.log(`Server running on port ${port}`));
