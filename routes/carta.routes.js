9f206b0d6e359cbc7d62a98901a12a4f25b5cf83
aalesann <alejandrosanabria74@gmail.com>
2022-02-28 01:49:44 -0300

    Pequeñas correcciones

--- routes/carta.routes.js
+++ routes/carta.routes.js
@@ -3,12 +3,17 @@ const path = require('path');
 const Visits = require('../models/Visits');
 
 router.get('/carta', async (req, res) => {
+    return res.sendFile(path.join(__dirname, '../public/carta.html'));
+});
+
+router.get('/sock', (req, res) =>{
     try {
         await Visits.updateOne({ name: 'exxim'}, {$inc: { visits: 1}});
     } catch (error) {
         console.error('Error al incrementar: ', error)
     }
-    return res.sendFile(path.join(__dirname, '../public/carta.html'));
+
+    return res.send('a');
 });
 
 module.exports = router;
